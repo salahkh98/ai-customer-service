c7573b091f40b7d7063d34567379efd0e5a1f079
salah khalil <salahyaseen484@gmail.com>
2024-07-14 03:49:28 +0300

    integrate genai instad openai with the webhook2233

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ async def handle_webhook(request: Request):
                             genai.configure(api_key='AIzaSyCvqGpzBDZBqbdz3huhYpPWejZC3u4_78s')
 
                             model = genai.GenerativeModel('gemini-pro')  # Use the Gemini Pro model
-                            response = model.generate_content(question_with_context,temperature=0.7)
+                            response = model.generate_content(question_with_context)
                             chatbot_response = response.text
 
                             # Combine chatbot response and product information
